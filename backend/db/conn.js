de862da947bb9a0408afc6e5e892f360d4f7ace7
Roberto <30988803+Hobyn@users.noreply.github.com>
2022-12-14 16:10:54 -0300

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -5,7 +5,7 @@ async function main() {
     mongoose.set("strictQuery", true);
 
     await mongoose.connect(
-      "mongodb+srv://roberto:ytKX35CnWXWqJNtZ@cluster0.d76tt6z.mongodb.net/?retryWrites=true&w=majority"
+      ""
     );
 
     console.log("Conectado ao banco!");
